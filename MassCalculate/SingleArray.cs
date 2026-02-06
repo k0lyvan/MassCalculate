023f11984894dd2b43b5e4a06815969e24a3f75a
MaximKol <mk.kolybanov@mail.ru>
2026-02-06 11:37:57 +0300

    Методы вычета массивов и операции массив - число

--- MassCalculate/SingleArray.cs
+++ MassCalculate/SingleArray.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MassCalculate
 {
@@ -35,7 +36,7 @@ namespace MassCalculate
             }
         }
 
-        static public int[] SumTwoArray(int[] arrayX, int[] arrayY)
+        static public int[] SumArray(int[] arrayX, int[] arrayY)
         {
             if (arrayX == null || arrayY == null) { return null; }
             else if (arrayX.Length != arrayY.Length) { return null; }
@@ -49,7 +50,7 @@ namespace MassCalculate
                 return sumArray;
             }
         }
-        static public double[] SumTwoArray(double[] arrayX, double[] arrayY)
+        static public double[] SumArray(double[] arrayX, double[] arrayY)
         {
             if (arrayX == null || arrayY == null) { return null; }
             else if (arrayX.Length != arrayY.Length) { return null; }
@@ -61,10 +62,22 @@ namespace MassCalculate
                     sumArray[i] = arrayX[i] + arrayY[i];
                 }
                 return sumArray;
+            }
+        }
+        static public double[] SumArray(double[] arrayX, double value)
+        {
+            if (arrayX == null || value == null) { return null; }
+            else
+            {
+                for (int i = 0; i < arrayX.Length; i++)
+                {
+                    arrayX[i] = arrayX[i] + value;
+                }
+                return arrayX;
             }
         }
 
-        static public int[] MultipkicationTwoArray(int[] arrayX, int[] arrayY)
+        static public int[] MultipkicationArray(int[] arrayX, int[] arrayY)
         {
             if (arrayX == null || arrayY == null) { return null; }
             else if (arrayX.Length != arrayY.Length) { return null; }
@@ -78,7 +91,7 @@ namespace MassCalculate
                 return sumArray;
             }
         }
-        static public double[] MultipkicationTwoArray(double[] arrayX, double[] arrayY)
+        static public double[] MultipkicationArray(double[] arrayX, double[] arrayY)
         {
             if (arrayX == null || arrayY == null) { return null; }
             else if (arrayX.Length != arrayY.Length) { return null; }
@@ -92,5 +105,59 @@ namespace MassCalculate
                 return sumArray;
             }
         }
+        static public double[] MultipkicationArray(double[] arrayX, double value)
+        {
+            if (arrayX == null || value == null) { return null; }
+            else
+            {
+                for (int i = 0; i < arrayX.Length; i++)
+                {
+                    arrayX[i] = arrayX[i] * value;
+                }
+                return arrayX;
+            }
+        }
+
+            static public int[] SubtractionArray(int[] arrayX, int[] arrayY)
+        {
+            if (arrayX == null || arrayY == null) { return null; }
+            else if (arrayX.Length != arrayY.Length) { return null; }
+            else
+            {
+                int[] sumArray = new int[arrayX.Length];
+                for (int i = 0; i < arrayX.Length; i++)
+                {
+                    sumArray[i] = arrayX[i] - arrayY[i];
+                }
+                return sumArray;
+            }
+        }
+        static public double[] SubtractionArray(double[] arrayX, double[] arrayY)
+        {
+            if (arrayX == null || arrayY == null) { return null; }
+            else if (arrayX.Length != arrayY.Length) { return null; }
+            else
+            {
+                double[] sumArray = new double[arrayX.Length];
+                for (int i = 0; i < arrayX.Length; i++)
+                {
+                    sumArray[i] = arrayX[i] - arrayY[i];
+                }
+                return sumArray;
+            }
+        }
+        static public double[] SubstractionArray(double[] arrayX, double value)
+        {
+            if (arrayX == null || value == null) { return null; }
+            else
+            {
+                for (int i = 0; i < arrayX.Length; i++)
+                {
+                    arrayX[i] = arrayX[i] - value;
+                }
+                return arrayX;
+            }
+        }
+
     }
 }
