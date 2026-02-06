a558ede663e9f968c03c0d9c0a55a05e4b8baff6
MaximKol <mk.kolybanov@mail.ru>
2026-02-06 11:26:30 +0300

    два метода сложения и умножения одномерных массивов

--- MassCalculate/SingleArray.cs
+++ MassCalculate/SingleArray.cs
@@ -16,7 +16,7 @@ namespace MassCalculate
                 int sum = 0;
                 foreach (int i in array)
                 {
-                    sum += i;                    
+                    sum += i;
                 }
                 return sum;
             }
@@ -32,6 +32,64 @@ namespace MassCalculate
                     sum += i;
                 }
                 return sum;
+            }
+        }
+
+        static public int[] SumTwoArray(int[] arrayX, int[] arrayY)
+        {
+            if (arrayX == null || arrayY == null) { return null; }
+            else if (arrayX.Length != arrayY.Length) { return null; }
+            else
+            {
+                int[] sumArray = new int[arrayX.Length];
+                for (int i = 0; i < arrayX.Length; i++)
+                {
+                    sumArray[i] = arrayX[i] + arrayY[i];
+                }
+                return sumArray;
+            }
+        }
+        static public double[] SumTwoArray(double[] arrayX, double[] arrayY)
+        {
+            if (arrayX == null || arrayY == null) { return null; }
+            else if (arrayX.Length != arrayY.Length) { return null; }
+            else
+            {
+                double[] sumArray = new double[arrayX.Length];
+                for (int i = 0; i < arrayX.Length; i++)
+                {
+                    sumArray[i] = arrayX[i] + arrayY[i];
+                }
+                return sumArray;
+            }
+        }
+
+        static public int[] MultipkicationTwoArray(int[] arrayX, int[] arrayY)
+        {
+            if (arrayX == null || arrayY == null) { return null; }
+            else if (arrayX.Length != arrayY.Length) { return null; }
+            else
+            {
+                int[] sumArray = new int[arrayX.Length];
+                for (int i = 0; i < arrayX.Length; i++)
+                {
+                    sumArray[i] = arrayX[i] * arrayY[i];
+                }
+                return sumArray;
+            }
+        }
+        static public double[] MultipkicationTwoArray(double[] arrayX, double[] arrayY)
+        {
+            if (arrayX == null || arrayY == null) { return null; }
+            else if (arrayX.Length != arrayY.Length) { return null; }
+            else
+            {
+                double[] sumArray = new double[arrayX.Length];
+                for (int i = 0; i < arrayX.Length; i++)
+                {
+                    sumArray[i] = arrayX[i] * arrayY[i];
+                }
+                return sumArray;
             }
         }
     }
